693dc4ba742632e4a354bc312dda7ff286fb579f
samuel maveke <mavekepeter2022@gmail.com>
2025-07-01 09:08:42 +0300

    deployment

--- server/controllers/addressController.js
+++ server/controllers/addressController.js
@@ -1,42 +1,25 @@
-import Address from "../models/Address.js"
+import Address from "../models/Address.js";
 
 //add Address :/api/address/add
-export const addAdress = async(req,res)=>{
-    try {
-        const {
-  firstName, lastName, email, street, city,
-  state, zipcode, country, phone, userId
-} = req.body;
-
-await Address.create({
-  firstName,
-  lastName,
-  email,
-  street,
-  city,
-  state,
-  zipcode,
-  country,
-  phone,
-  userId
-});
-
-        res.json({success:true,message:"Address added successfully"})
-
-    } catch (error) {
-        console.log(error.message);
-        res.json({success:false,message:error.message})
-    }
-}
+export const addAdress = async(req, res) => {
+  try {
+    const addressData = req.body; // receives flattened address + userId
+    await Address.create(addressData);
+    res.json({ success: true, message: "Address added successfully" });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //get Address : /api/address/get
-export const getAddress = async(req,res)=>{
-    try {
-        const {userId} = req.query;
-        const addresses = await Address.find({userId});
-        res.json({success:true,addresses})
-    } catch (error) {
-        console.log(error.message);
-        res.json({success:false,message:error.message})
-    }
-}
+export const getAddress = async (req, res) => {
+  try {
+    const { userId } = req.query;
+    const addresses = await Address.find({ userId });
+    res.json({ success: true, addresses });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
